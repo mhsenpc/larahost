4e270d02480e00e5a87ce184bd058ba882bf5889
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-06-13 12:42:13 +0430

    more debug info

--- app/Services/DockerService.php
+++ app/Services/DockerService.php
@@ -4,9 +4,11 @@
 namespace App\Services;
 
 
+use Illuminate\Support\Facades\Log;
+
 class DockerService
 {
-    protected $binary = "docker";
+    protected $binary = "/usr/bin/docker";
 
     public function __construct() {
         if (config("app.env") == "local") {
@@ -15,7 +17,12 @@ class DockerService
     }
 
     public function newLaravelContainer(string $name, int $port,string $project_dir) {
-        exec( "{$this->binary} run -v $project_dir:/var/www/html --name=$name -d -p $port:80  bitnami/laravel:8");
+        Log::debug("trying to docker run $project_dir on port $port");
+        exec( "{$this->binary} run -v $project_dir:/var/www/html --name=$name -d -p $port:80  bitnami/laravel:8 2>&1", $output, $return_var);
+        Log::debug("output is ");
+        Log::debug($output);
+        Log::debug("return var is ");
+        Log::debug($return_var);
     }
 
     protected function writeDockerCompose(){
