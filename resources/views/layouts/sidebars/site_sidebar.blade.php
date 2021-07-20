4f4c96de715b10d5ef43f5936ead59637f93af7f
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-07-20 17:10:53 +0430

    Add ir flag and goto button

--- resources/views/layouts/sidebars/site_sidebar.blade.php
+++ resources/views/layouts/sidebars/site_sidebar.blade.php
@@ -1,41 +1,49 @@
 <!-- Sidebar user panel -->
 <div class="user-panel">
     <div class="pull-right image">
-        <img src="{{asset('dist/img/server.png')}}"  />
+        <img src="{{asset('dist/img/server.png')}}"/>
     </div>
     <div class="pull-right info">
-        <p>{{$site->name}}</p>
+        <p>
+            {{$site->name}}
+            <a target="_blank" href="http://{{$site->name}}.gnulover.ir">
+                <i class="fa fa-arrow-circle-right text-green"></i>
+            </a>
+        </p>
         @if($running)
-        <a ><i class="fa fa-circle text-success"></i> روشن</a>
+            <a><i class="fa fa-circle text-success"></i> روشن</a>
         @else
-            <a  ><i class="fa fa-circle text-danger"></i> خاموش</a>
+            <a><i class="fa fa-circle text-danger"></i> خاموش</a>
         @endif
+    </div>
+    <div class="pull-left">
+        <img src="{{asset('dist/img/iran_flag.png')}}" width="50em" />
     </div>
 </div>
 <!-- sidebar menu: : style can be found in sidebar.less -->
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header">منو</li>
-    <li >
+    <li>
         <a href="{{route('sites.index')}}">
             <i class="fa fa-arrow-right"></i> <span>بازگشت به لیست سایت ها</span>
         </a>
     </li>
-    <li >
+    <li>
         <a href="{{route('sites.show',['site'=>$site])}}">
             <i class="fa fa-circle"></i> <span>میزکار سایت</span>
         </a>
     </li>
-    <li >
+    <li>
         <a href="{{route('sites.env_editor',['site'=>$site])}}">
             <i class="fa fa-circle"></i> <span>Environment</span>
         </a>
     </li>
-    <li >
+    <li>
         <a href="{{route('sites.deployments',['site'=>$site])}}">
             <i class="fa fa-circle"></i> <span>Deployments</span>
         </a>
     </li>
-    <li >
+    <li>
         <a href="{{route('sites.logs',['site'=>$site])}}">
             <i class="fa fa-circle"></i> <span>Laravel Logs</span>
         </a>
