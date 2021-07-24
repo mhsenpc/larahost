266344bf9d66d8c58633fe4227add9691367c66a
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-07-24 10:59:36 +0430

    format deploy log

--- resources/views/site/show_deployment_log.blade.php
+++ resources/views/site/show_deployment_log.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.single_box')
-@php($title="گزارش Deployment | ".$deployment->created_at)
+@php($title="گزارش Deployment")
 @php($sidebar="layouts.sidebars.site_sidebar")
+@php($description=$deployment->created_at)
 
 @section('box_content')
     @include('site.last_deployment_log')
