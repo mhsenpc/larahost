986f330510f401c6b0b15efd9058ce8f21dc9e51
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-07-25 13:33:50 +0430

    git clone using ssh key

--- config/larahost.php
+++ config/larahost.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'repos_dir' => env('REPOS_DIR', '/home/repos'),
-    'keys_dir' => env('REPOS_DIR', '/home/keys'),
+    'keys_dir' => env('KEYS_DIR', '/home/keys'),
     'super_user_api_url' => 'http://127.0.0.1:10000',
     'dir_names' => [
         'docker-compose' => 'docker-compose',
