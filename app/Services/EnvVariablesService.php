8587414c3c784fb0e7547ae8729d4ff36cf259f0
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-08-01 11:09:09 +0430

    bug: base url is 127.0.0.1

--- app/Services/EnvVariablesService.php
+++ app/Services/EnvVariablesService.php
@@ -11,12 +11,12 @@ class EnvVariablesService
 {
     protected $connection_info;
     protected $env_path;
-    private $project_name;
+    private $site_name;
 
-    public function __construct(string $source_dir, string $project_name, ConnectionInfo $connection_info) {
+    public function __construct(string $source_dir, string $site_name, ConnectionInfo $connection_info) {
         $this->connection_info = $connection_info;
         $this->env_path        = $source_dir;
-        $this->project_name    = $project_name;
+        $this->site_name    = $site_name;
         if (file_exists($source_dir . "/.env")) {
             // use their env in repo. however awkward it is
         } else if (file_exists($source_dir . "/.env.example")) {
@@ -32,9 +32,9 @@ class EnvVariablesService
         $env_editor = new DotEnvEditor($this->env_path);
 
         // app name
-        $env_editor->changeKey('APP_NAME', $this->project_name);
+        $env_editor->changeKey('APP_NAME', $this->site_name);
 
-        $env_editor->changeKey('APP_URL', "http://{$this->project_name}.gnulover.ir");
+        $env_editor->changeKey('APP_URL', "http://{$this->site_name}.gnulover.ir");
 
         // db connection info
         $env_editor->changeKey('DB_CONNECTION', $this->connection_info->db_connection);
@@ -43,6 +43,9 @@ class EnvVariablesService
         $env_editor->changeKey('DB_DATABASE', $this->connection_info->db_name);
         $env_editor->changeKey('DB_USERNAME', $this->connection_info->db_username);
         $env_editor->changeKey('DB_PASSWORD', $this->connection_info->db_password);
+        $env_editor->changeKey('CACHE_DRIVER', 'redis');
+        $env_editor->changeKey('SESSION_DRIVER', 'redis');
+        $env_editor->changeKey('REDIS_HOST', "{$this->site_name}_redis");
         $env_editor->flush();
     }
 }
