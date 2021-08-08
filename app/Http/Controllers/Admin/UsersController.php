d04a30b5159048924adfc72dfe8913bd60402171
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-08-08 14:09:07 +0430

    pagination perpage

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Auth;
 
 class UsersController {
     public function index(){
-        $users = User::query()->orderBy('created_at','desc')->paginate(5);
+        $users = User::query()->orderBy('created_at','desc')->paginate();
         return view('admin.users',compact('users'));
     }
 
