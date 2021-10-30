8f8786f276543774a5bc353c964ca69142f468eb
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-10-30 18:29:49 +0330

    debug inspect

--- app/Services/ContainerInfoService.php
+++ app/Services/ContainerInfoService.php
@@ -5,12 +5,15 @@ namespace App\Services;
 
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class ContainerInfoService {
     public static function getPowerStatus(string $container_name): bool {
         if(App::environment('local'))
             return true;
         $info = SuperUserAPIService::inspect($container_name);
+        Log::debug('inspect is');
+        Log::debug(print_r($info,true));
         if($info['success']){
             return $info['data'][0]['State']['Status'] == "running";
         }
