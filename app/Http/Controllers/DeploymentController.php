a0edeea618ef5871a369ef5baaac2c3f42befc56
Mohsen Shamohamadi <mhsenpc@gmail.com>
2021-11-16 19:19:24 +0330

    fix getlogsdir

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -15,8 +15,8 @@ class DeploymentController extends Controller
             abort(403);
         }
         $site = $deployment->site;
-        $deployment_logs_dir = $site->getDeploymentLogsDir();
-        $log_content = file_get_contents($deployment_logs_dir .'/'. $deployment->log_file);
+        $siteObj = new \App\Services\Site($site);
+        $log_content = file_get_contents($siteObj->getFilesystem()->getDeploymentLogsDir() .'/'. $deployment->log_file);
         return view('site.show_deployment_log',compact('log_content','site','deployment'));
     }
 
@@ -25,7 +25,8 @@ class DeploymentController extends Controller
         if(empty($deployment)){
             return response('تاکنون برای این سایت هیچ deployment ای انجام نشده است');
         }
-        $log_content = file_get_contents($deployment->site->getDeploymentLogsDir() .'/'. $deployment->log_file);
+        $siteObj = new \App\Services\Site($deployment->site);
+        $log_content = file_get_contents($siteObj->getFilesystem()->getDeploymentLogsDir() .'/'. $deployment->log_file);
         return view('site.last_deployment_log',compact('log_content'));
     }
 }
