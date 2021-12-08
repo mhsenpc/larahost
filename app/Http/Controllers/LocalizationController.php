b778344bc9a00c5a53e3b3b4c27aaf795b07a142
عبدالستار نظری <67092431+Target-Zone@users.noreply.github.com>
2021-12-08 13:54:39 +0330

    Feat/translation (#2)
    * translation files for english and persian
    
    * inserting the key for translating... last directory site/domain
    
    * adding remained untranslated key value for message.php file, adding all key for blade expecting the admin folder, adding language choice button for login and relavent pages
    
    * translation for site/admin folder, insert remained translation view blade file, fix some problem in translation key, change app.php value of lang key, noted remained problem
    
    * commented (removed) the scandir() function for logs array and relavented used of logs variabled code, then created a empty logs array.. correct translation command and show  blade pages in view...
    
    * correct swich lang button with adding route function for app,register,reset,email,login blade view files. modifie route web.php. add lang.css, correct quicky for message translation word in en
    
    * remove comment for logs array, remove empty logs array
    
    * change some messages
    
    * remove session local variable, declare App:locale class for locale variable into app, register, reset, email, login blade files
    
    * bug fix translation for App class declaration
    
    Co-authored-by: Mohsen Shamohamadi <mhsenpc@gmail.com>
    Co-authored-by: Mohsen Shamohammadi <mhsenpc@yahoo.com>

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\support\facades\App;
+use Illuminate\Support\Facades\App;
 
 class LocalizationController extends Controller
 {
     public function index($locale)
     {
         App::setlocale($locale);
-        session()->put('locale',$locale);        
+        session()->put('locale',$locale);
         return redirect()->back();
     }
 }
