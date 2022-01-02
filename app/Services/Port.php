4963eff812fe6b1674339c23a50cb2006c4761da
عبدالستار نظری <67092431+Target-Zone@users.noreply.github.com>
2022-01-02 17:03:02 +0330

    Feat/portconfig behavior (#11)
    * deploy script fixing for demo user deny permission
    
    * create starting port in envirenment then add it for config/larahost.php, therefore add starting port for services/port.php
    
    * change the port number of starting port condition in port.php
    
    * hopfully haven't more conflict syntax occuring to for_loop, tested the returned syntax in oncompiler.com

--- app/Services/Port.php
+++ app/Services/Port.php
@@ -6,17 +6,19 @@ namespace App\Services;
 
 use App\Exceptions\NoPortsAvailableException;
 use App\Models\Site;
+use Config\Larahost;
 
 class Port {
     protected $reserved_ports = [21, 22, 23, 80, 443, 3306, 8000, 9000, 9001, 10000];
-
     public function next():int {
+        $starting_port = config('larahost.starting_port.port');
         $busy_ports = $this->getBusyPorts();
-        for ($port = 10001; $port < 15000; $port++) {
+		for ( $port = $starting_port ; $port < $starting_port + 5000; $port++)
+		{
             if (!in_array($port, $busy_ports)) {
                 return $port;
             }
-        }
+		}
         throw new NoPortsAvailableException();
     }
 
